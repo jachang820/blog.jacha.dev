67ab5af6f82159fb8ded6f6cb423d899f7d13c69
jachang820 <j.a.chang820@gmail.com>
2025-01-03 18:21:21 -0800

    Nav bar styling

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Thoughts.toString()';
+export const SITE_DESCRIPTION = 'This is a blog where I write down things that I have learned related to software and tech!';
